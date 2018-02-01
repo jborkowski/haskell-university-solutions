4f624d77ba7caaf3299e1b6fa5b146a1406cfde1
Jonatan Borkowski <jonatanborkowski@wp.pl>
2018-02-01 14:30:47 +0100

    Bug fix - Nested Right

--- ex5/app/Main.hs
+++ ex5/app/Main.hs
@@ -15,8 +15,6 @@ appState :: StateT (Map.Map String String) IO ()
 appState = do
   line         <- lift getLine
   resultsState <- get -- get from state
-  let replacedS =  replaceAllResults line resultsState
-  lift (print replacedS) -- debug only
   let result = do
         replaced <- replaceAllResults line resultsState
         result   <- addErrorMsg (solveEquation (unwords replaced)) "Can't calculate result of provided equation"
@@ -26,7 +24,7 @@ appState = do
     Left msg  -> lift (print msg)
     Right res -> do
       let newKey = "res" ++ show (Map.size resultsState)
-      put (Map.insert newKey (show result) resultsState)   
+      put (Map.insert newKey (show res) resultsState)   
       lift (print (show res))
 
   -- TODO: parser - check that imput not waiting for result from state [DONE],
@@ -47,12 +45,12 @@ replaceRes w m = map (\e -> if isPrefixOf "res" e then Map.findWithDefault e e m
 
 replaceAllResults :: String -> ResultMap -> Either String [String]
 replaceAllResults input resMap = 
-    -- mapM (\i -> if isPrefixOf "res" i then resultLookup i resMap else Right i) . words $ input
-    traverse fn . words $ input
-    where fn i | isPrefixOf "res" i = resultLookup i resMap
-               | otherwise          = Right i   
+    mapM (\i -> if isPrefixOf "res" i then resultLookup i resMap else Right i) . words $ input
+    --traverse fn . words $ input
+    -- where fn i | isPrefixOf "res" i = resultLookup i resMap
+    --           | otherwise          = return i   
 
 resultLookup :: String -> ResultMap -> Either String String
 resultLookup resId resMap = case Map.lookup resId resMap of
   Nothing  -> Left ("Result " ++ show resId ++ "doesn't exist")
-  Just res -> Right res
+  Just res -> return res
