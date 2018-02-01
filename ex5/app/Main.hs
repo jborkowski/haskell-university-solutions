366139e9791bc7969a0d7da3b2e8ef039ce6deb2
Jonatan Borkowski <jonatanborkowski@wp.pl>
2018-02-01 21:39:18 +0100

    refacored

--- ex5/app/Main.hs
+++ ex5/app/Main.hs
@@ -7,19 +7,21 @@ import Data.List
 import Control.Monad
 import Control.Monad.State
 import ReversePolishNotationWithState
+import System.Console.Haskeline
 
 main :: IO (a, Map.Map String String)
 main = runStateT (forever appState) Map.empty
 
 appState :: StateT (Map.Map String String) IO ()
 appState = do
+  --            <- lift (print "> ")
   line         <- lift getLine
   resultsState <- get -- get from state
   let result = do
         replaced <- replaceAllResults line resultsState
         result   <- addErrorMsg (solveEquation (unwords replaced)) "Can't calculate result of provided equation"
         return result
-  
+        
   case result of 
     Left msg  -> lift (print msg)
     Right res -> do
@@ -27,30 +29,3 @@ appState = do
       put (Map.insert newKey (show res) resultsState)   
       lift (print (show res))
 
-  -- TODO: parser - check that imput not waiting for result from state [DONE],
-  -- TODO: add result to the state [DONE],
-  -- TODO: add error handling - parser, error calculation, and messages[INPROGRESS]
-  -- TODO: clean up code! names refactor
-
-type Result = String
-type ResultMap = Map.Map String Result
-
-addErrorMsg :: Maybe a -> String -> Either String a
-addErrorMsg mayA errorMsg = case mayA of
-  Nothing -> Left errorMsg
-  Just a  -> Right a
-
-replaceRes :: [String] -> ResultMap -> [String]
-replaceRes w m = map (\e -> if isPrefixOf "res" e then Map.findWithDefault e e m else e) w
-
-replaceAllResults :: String -> ResultMap -> Either String [String]
-replaceAllResults input resMap = 
-    mapM (\i -> if isPrefixOf "res" i then resultLookup i resMap else Right i) . words $ input
-    --traverse fn . words $ input
-    -- where fn i | isPrefixOf "res" i = resultLookup i resMap
-    --           | otherwise          = return i   
-
-resultLookup :: String -> ResultMap -> Either String String
-resultLookup resId resMap = case Map.lookup resId resMap of
-  Nothing  -> Left ("Result " ++ show resId ++ "doesn't exist")
-  Just res -> return res
--- ex5/src/ReversePolishNotationWithState.hs
+++ ex5/src/ReversePolishNotationWithState.hs
@@ -1,9 +1,21 @@
 module ReversePolishNotationWithState
   ( solveEquation
+  , replaceAllResults
+  , addErrorMsg
   ) where
 
+import qualified Data.Map.Strict as Map
+import Data.List
+import Control.Monad
+import Control.Monad.State
 import Control.Applicative
 import Text.Read
+
+type Result = String
+type ResultMap = Map.Map String Result
+
+type Digit = Double
+ 
 
 solveEquation :: String -> Maybe Double
 solveEquation = head . foldl fn [] . words
@@ -13,4 +25,20 @@ solveEquation = head . foldl fn [] . words
         fn (a:b:acc) "/" = (pure (/) <*> a <*> b) : acc
         fn (a:b:acc) "*" = (pure (*) <*> a <*> b) : acc
         fn acc item      = readMaybe item:acc
-         
+
+addErrorMsg :: Maybe a -> String -> Either String a
+addErrorMsg mayA errorMsg = case mayA of
+  Nothing -> Left errorMsg
+  Just a  -> Right a
+
+replaceRes :: [String] -> ResultMap -> [String]
+replaceRes w m = map (\e -> if isPrefixOf "res" e then Map.findWithDefault e e m else e) w
+
+replaceAllResults :: String -> ResultMap -> Either String [String]
+replaceAllResults input resMap = 
+    mapM (\i -> if isPrefixOf "res" i then resultLookup i resMap else Right i) . words $ input  
+
+resultLookup :: String -> ResultMap -> Either String String
+resultLookup resId resMap = case Map.lookup resId resMap of
+  Nothing  -> Left ("Result " ++ show resId ++ "doesn't exist")
+  Just res -> return res
